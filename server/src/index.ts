61acb648e099b4b6f5595cb2978c296dedaa2230
Cahil Foley <cahilfoley2@gmail.com>
2020-07-13 16:53:45 +0800

    update static server to handle SPA hosting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import { createServer } from 'http'
+import path from 'path'
 import express from 'express'
 import { attachSubscriptionServer, attachGraphQLRoutes } from './graphql'
 // import { getConnection } from './storage/connection'
 
 const PORT = process.env.PORT || 8080
+
+const userAgentsToMatch = /msie|trident/i
 
 async function start() {
   // Start database connection
@@ -12,7 +15,20 @@ async function start() {
   const app = express()
 
   await attachGraphQLRoutes(app)
-  app.get('/', (req, res) => res.send('Hello World'))
+  app.use('/static', express.static('./public/static', { maxAge: 31536000 }))
+  app.use('/', express.static('./public', { maxAge: 0 }))
+
+  app.get('*', (req, res, next) => {
+    if (
+      req.headers.accept?.toLowerCase().includes('text/html') ||
+      userAgentsToMatch.test(req.headers['user-agent'] || '')
+    ) {
+      const indexPath = path.join(process.cwd(), 'public/index.html')
+      return res.sendFile(indexPath)
+    } else {
+      next()
+    }
+  })
 
   const server = createServer(app)
 
