75b47ecb2da67b946fa96ad22b898d0829dedc46
Cahil Foley <cahilfoley2@gmail.com>
2020-07-13 16:54:08 +0800

    improve TypedEmitter to expose additional event emitter methods

--- server/src/internal/TypedEmitter.ts
+++ server/src/internal/TypedEmitter.ts
@@ -3,28 +3,40 @@ import { EventEmitter } from 'events'
 type EventMap = Record<string, any>
 
 type EventType<T extends EventMap> = string & keyof T
-type EventHandler<T> = (params: T) => void
+type EventHandler<T> = (payload: T) => void
 
 interface Emitter<T extends EventMap> {
-  on<K extends EventType<T>>(event: K, handler: EventHandler<T[K]>): void
-  off<K extends EventType<T>>(event: K, handler: EventHandler<T[K]>): void
-  emit<K extends EventType<T>>(event: K, params: T[K]): void
+  on<K extends EventType<T>>(event: K, handler: EventHandler<T[K]>): this
+  off<K extends EventType<T>>(event: K, handler: EventHandler<T[K]>): this
+  once<K extends EventType<T>>(event: K, payload: T[K]): this
+  emit<K extends EventType<T>>(event: K, payload: T[K]): boolean
+  removeAllListeners(): this
 }
 
 export class TypedEmitter<T extends EventMap> implements Emitter<T> {
-  private emitter = new EventEmitter()
+  #emitter = new EventEmitter()
 
   on<K extends EventType<T>>(event: K, handler: EventHandler<T[K]>) {
-    this.emitter.on(event, handler)
+    this.#emitter.on(event, handler)
     return this
   }
 
   off<K extends EventType<T>>(event: K, handler: EventHandler<T[K]>) {
-    this.emitter.off(event, handler)
+    this.#emitter.off(event, handler)
+    return this
+  }
+
+  once<K extends EventType<T>>(event: K, handler: EventHandler<T[K]>) {
+    this.#emitter.once(event, handler)
     return this
   }
 
   emit<K extends EventType<T>>(event: K, payload: T[K]) {
-    this.emitter.emit(event, payload)
+    return this.#emitter.emit(event, payload)
+  }
+
+  removeAllListeners() {
+    this.#emitter.removeAllListeners()
+    return this
   }
 }
